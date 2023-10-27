76e2048255aa0ffc2975a2eee6d41777c7f24d5d
polakikiran <polakisaikiran2@gmail.com>
2023-10-28 00:44:33 +0530

    added new contributors page

---
+++ contributors/script.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const contributorsContainer = document.getElementById(
+    "contributors-container"
+  );
+
+  fetch("https://api.github.com/repos/KiranPolaki/GuessTheNumber/contributors")
+    .then((response) => response.json())
+    .then((data) => {
+      data.forEach((contributor) => {
+        const contributorDiv = document.createElement("div");
+        contributorDiv.classList.add("contributor");
+
+        const contributorLink = document.createElement("a");
+        contributorLink.href = contributor.html_url;
+        contributorLink.target = "_blank";
+        contributorLink.rel = "noopener noreferrer";
+
+        const img = document.createElement("img");
+        img.src = contributor.avatar_url;
+        img.alt = contributor.login;
+
+        const h3 = document.createElement("h3");
+        h3.textContent = contributor.login;
+
+        contributorLink.appendChild(img);
+        contributorLink.appendChild(h3);
+        contributorDiv.appendChild(contributorLink);
+        contributorsContainer.appendChild(contributorDiv);
+      });
+    });
+});
